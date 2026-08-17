6e4f8a960a1e5f925838f8a0743a74df74e71700
mondalGourish <gourishmondal02@gmail.com>
2026-08-17 19:47:43 +0530

    setup for tailwind css

---
+++ 05_css/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Hi i am gourish</h1>
+      <button>Button</button>
+    </div>
+  )
+}
+
+export default Header
